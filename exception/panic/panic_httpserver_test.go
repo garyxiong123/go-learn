157fc46ad80f7da6d80bb77c47a888518114a55c
garyxiong123 <502238410@qq.com>
2023-02-01 02:39:24 +0800

    123

--- exception/panic/panic_httpserver_test.go
+++ exception/panic/panic_httpserver_test.go
@@ -5,9 +5,7 @@ import (
 	"testing"
 )
 
-const requestIDKey int = 0
-
-func WithRequestID(next http.Handler) http.Handler {
+func newHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(rw http.ResponseWriter, req *http.Request) {
 			// 调用 HTTP 处理函数
@@ -17,15 +15,13 @@ func WithRequestID(next http.Handler) http.Handler {
 }
 
 func Handle(rw http.ResponseWriter, req *http.Request) {
-	// 拿到 reqId，后面可以记录日志等等
 	panic("sss")
-
 }
 
 // http://localhost:8899/
 
 func Test_Get_Http_Id(t *testing.T) {
 
-	handler := WithRequestID(http.HandlerFunc(Handle))
+	handler := newHandler(http.HandlerFunc(Handle))
 	http.ListenAndServe("localhost:8899", handler)
 }
--- web/go-zero/error_code/error_code_main.go
+++ web/go-zero/error_code/error_code_main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/garyxiong123/go-learn/web/go-zero/common/errorx"
 	"github.com/garyxiong123/go-learn/web/go-zero/error_code/internal/config"
 	"github.com/garyxiong123/go-learn/web/go-zero/error_code/internal/handler"
@@ -28,17 +27,20 @@ func main() {
 
 	handler.RegisterHandlers(server, ctx)
 
-	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
+	//fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 
 	// 全局中间件
-	//server.Use(func(next http.HandlerFunc) http.HandlerFunc {
-	//	return func(writer http.ResponseWriter, request *http.Request) {
-	//		println("gary execute before -----------")
-	//		next(writer, request)
-	//		println("gary execute after -----------", []byte("sss"))
-	//
-	//	}
-	//})
+	server.Use(func(next http.HandlerFunc) http.HandlerFunc {
+
+		return func(writer http.ResponseWriter, request *http.Request) {
+			println("gary execute before -----------")
+
+			next(writer, request) //filter
+
+			println("gary execute after -----------", []byte("sss"))
+
+		}
+	})
 
 	http.Handle("/swagger/", http.StripPrefix("/swagger/", http.FileServer(http.Dir("swagger-ui-dist"))))
 
