3dc55faa0c52bdb11157c2985233c58b8a85be4f
garyxiong123 <502238410@qq.com>
2023-02-01 03:07:38 +0800

    123

--- cmd/flags/flags.go
+++ cmd/flags/flags.go
@@ -6,9 +6,8 @@ import (
 
 var (
 	ConfigFlag = &cli.StringFlag{
-		Name:    "config",
-		Aliases: []string{"f"},
-		Usage:   "the config file",
+		Name:  "config",
+		Usage: "the config file",
 	}
 	ContractAddrFlag = &cli.StringFlag{
 		Name:  "contractAddr",
