9e4bb305117d63c5b559ebdc0f47b91c8f1b059e
toni.x <toni.x@binance.com>
2023-02-01 23:40:15 +0300

    123

--- 4_db/orm/exception/1_exp_select_test.go
+++ 4_db/orm/exception/1_exp_select_test.go
@@ -8,25 +8,12 @@ import (
 	"testing"
 )
 
-func Test_select(t *testing.T) {
-	Db.Create(&basic.User{Name: "toni"})
-	user := &basic.User{}
-	tx := Db.Where("name = ?", "toni").First(user)
-	if tx.Error != nil {
-		fmt.Println(tx.Error)
-	}
-	if tx.RowsAffected == 0 {
-		fmt.Println(0)
-	}
-	fmt.Println(user)
-}
-
 // 当 First、Last、Take 方法找不到记录时，GORM 会返回 ErrRecordNotFound 错误。
 // 如果发生了多个错误，你可以通过 errors.Is 判断错误是否为 ErrRecordNotFound，例如：
 func Test_exp_first(t *testing.T) {
 	user := &basic.User{}
 	tx := Db.Table("users").Where("name = ?", "toni").First(user)
-	fmt.Println(errors.Is(tx.Error, gorm.ErrRecordNotFound))
+	fmt.Println(errors.Is(tx.Error, gorm.ErrRecordNotFound)) //包含各种错误类型
 }
 
 func Test_exp_find(t *testing.T) {
